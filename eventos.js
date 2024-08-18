13fade3a500e140797eae01e9c7a7c3d777438ab
MicaelaRamosGonzalez <micaelaramosgonzalez@gmail.com>
2024-08-18 02:16:45 -0300

    Solucion de problema de click

--- eventos.js
+++ eventos.js
@@ -6,3 +6,17 @@ div.addEventListener('click', function (){
     alert ("Hola soy el div");
 });
 // div.addEventListener lo que hace es decirle que agregue la funcion cuando el evento ocurra.En este caso la function es el mensaje "Hola soy el div"
+
+
+var button = document.getElementById("saludo");
+
+// Agrego el manejador de eventos para el clic en el botón
+button.addEventListener('click', function(event) {
+    event.stopPropagation(); // Detener la propagación del evento hacia el div
+});
+    
+    
+    
+    
+    
+    
